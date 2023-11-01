e0044638846ff3b6a152595f9d7c880986e7f3c2
HseinKt <hseinkteish1998@gmail.com>
2023-11-01 21:26:06 +0200

    added all models

--- back_end/app/Models/Flight.php
+++ back_end/app/Models/Flight.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Flight extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'departure_airport',
+        'arrival_airport',
+        'departure_time',
+        'adults',
+        'children',
+        'price',
+        'flight_type',
+        'round_trip_flight_id',
+        'multi_city_flight_id',
+        'created_at',
+        'updated_at',
+    ];
 }
